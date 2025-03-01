87637d4ea7c97fb2dfbd483c627d2ff466228e3f
oscar <oscarf1030p@gmail.com>
2025-03-01 21:17:16 +0100

    Barra de vida y armadura

--- src/UI/PlayerUI.js
+++ src/UI/PlayerUI.js
@@ -6,23 +6,81 @@ export default class PlayerUI extends Phaser.GameObjects.Container {
     static UI_MARGIN_X = 10;
     static UI_MARGIN_Y = 10;
 
+    static BARRA_WIDTH = 5;
+    static BARRA_HEIGHT = 16;
+
+    static POS_X = 65;
+    static POS_VIDA_Y = 66;
+    static POS_ESCUDO_Y = 34;
+
+    static COLOR_BARRA_VIDA = 0xff0000;
+    static COLOR_BARRA_ESCUDO = 0x00007a;
+
+    static MAX_NUM_RECTANGLES = 40;
+
     constructor(scene, x, y){
         super(scene, x, y)
         this.scene.add.existing(this);
-        
+
         this._barraVida = new Phaser.GameObjects.Sprite(scene, x, y, 'playerUI')
         this._barraVida.setScale(2)
         this._barraVida.setOrigin(0)
         this._barraVida.setScrollFactor(0)
-        this.add(this._barraVida)
-
         this.setPosition(PlayerUI.UI_MARGIN_X, PlayerUI.UI_MARGIN_Y)
+
+        this._puntosDeVida = this.crear_barra_vida()
+        this._puntosDeArmadura = this.crear_barra_escudo()
+
+        this.add(this._barraVida)
+        this.add(this._puntosDeVida)
+        this.add(this._puntosDeArmadura)
     }
 
+    crear_barra_vida(){
 
-    update(){
-        super.update();
-        console.log('Aquii')
+        var barra = []
+        for(let i = 0; i < PlayerUI.MAX_NUM_RECTANGLES; i++){
+
+            const rectangle = this.scene.add.rectangle(
+                PlayerUI.POS_X + i*8, 
+                PlayerUI.POS_VIDA_Y, 
+                PlayerUI.BARRA_WIDTH, 
+                PlayerUI.BARRA_HEIGHT, 
+                PlayerUI.COLOR_BARRA_VIDA)
+            rectangle.setOrigin(0)
+            rectangle.setScrollFactor(0)
+            barra.push(rectangle)
+        }
+
+        return barra;
     }
+
+    crear_barra_escudo(){
+
+        var barra = []
+        for(let i = 0; i < PlayerUI.MAX_NUM_RECTANGLES; i++){
+
+            const rectangle = this.scene.add.rectangle(
+                PlayerUI.POS_X + i*8, 
+                PlayerUI.POS_ESCUDO_Y, 
+                PlayerUI.BARRA_WIDTH, 
+                PlayerUI.BARRA_HEIGHT, 
+                PlayerUI.COLOR_BARRA_ESCUDO)
+            rectangle.setOrigin(0)
+            rectangle.setScrollFactor(0)
+            barra.push(rectangle)
+        }
+
+        return barra;
+    }
+
+    aumentar_puntos(barra, cantidad){
+        
+    }
+
+    disminuir_puntos(barra, cantidad){
+
+    }
+
 
 }
--- src/base-game-objects/RangeWeapon.js
+++ src/base-game-objects/RangeWeapon.js
@@ -28,28 +28,33 @@ export default class RangeWeapon extends Weapon {
     }
 
     shot(targetX, targetY) {
-        const now = this.scene.time.now; // Obtener tiempo actual
+
+        const now = this.scene.time.now;
+
+        // Comprobar si puede volver a disparar(cadencia de disparo) y si tiene municion
+        if(now - this._lastShotTime < this._specs.fireRate * 1000 || this._ammo.currentClipAmmo <= 0)
+            return;
+
         
-        if (now - this._lastShotTime >= this._specs.fireRate * 1000) { // Comprobación del cooldown
-            if (this._ammo.currentClipAmmo > 0) {
-                this._lastShotTime = now; // Actualizar el último tiempo de disparo
-                this._ammo.currentClipAmmo--; // Restar una bala
+        this._lastShotTime = now;
+        this._ammo.currentClipAmmo--;
+ 
+        // Obtener la posición mundial del arma
+        const weaponX = this.parentContainer.x + this.x;
+        const weaponY = this.parentContainer.y + this.y;
 
-                // Obtener la posición mundial del arma
-                const weaponX = this.parentContainer.x + this.x;
-                const weaponY = this.parentContainer.y + this.y;
+        // Calcular el ángulo de disparo
+        const angle = Phaser.Math.Angle.Between(weaponX, weaponY, targetX, targetY);
+        const bulletX = weaponX + Math.cos(angle) * this._specs.muzzleOffset;
+        const bulletY = weaponY + Math.sin(angle) * this._specs.muzzleOffset;
 
-                // Calcular el ángulo de disparo
-                const angle = Phaser.Math.Angle.Between(weaponX, weaponY, targetX, targetY);
-                const bulletX = weaponX + Math.cos(angle) * this._specs.muzzleOffset;
-                const bulletY = weaponY + Math.sin(angle) * this._specs.muzzleOffset;
+        // Crear la bala en la posición del arma
+        const bullet = new BasePistolBullet(this.scene, bulletX, bulletY);
+        this.scene.add.existing(bullet);
+        this.scene.bullets.add(bullet);
+        bullet.fire(bulletX, bulletY, angle, this._specs.bulletSpeed);
 
-                // Crear la bala en la posición del arma
-                const bullet = new BasePistolBullet(this.scene, bulletX, bulletY);
-                this.scene.add.existing(bullet);
-                this.scene.bullets.add(bullet);
-                bullet.fire(bulletX, bulletY, angle, this._specs.bulletSpeed);
-            }
-        }
     }
+
+
 }
